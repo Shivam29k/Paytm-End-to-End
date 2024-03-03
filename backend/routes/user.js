03d5a581802f94e34f44f513497f2d8de4ffafa5
Shivam Kumar <skumarshivam50@gmail.com>
2024-03-04 00:30:09 +0530

    bug fix

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -169,8 +169,8 @@ router.get("/bulk", async (req, res) => {
     res.json({
         user: users.map(user => ({
             username: user.username,
-            firstName: user.firstName,
-            lastName: user.lastName,
+            firstName: user.firstname,
+            lastName: user.lastname,
             _id: user._id
         }))
     })
