e97b7f01cc3f8cef40a2b6cf877e8fb3e7826481
Shivam Kumar <skumarshivam50@gmail.com>
2024-03-04 07:24:35 +0530

    commit

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -5,8 +5,8 @@ function Home() {
     <div className="flex items-center justify-center gap-5 h-screen">
         <div className="flex items-center justify-center gap-10 m-10 flex-col ">
         <div className="text-3xl w-64 text-center font-bold">Welcome to Payments app</div>
-        <a href="/signup"><button className="button-50">Sign Up</button></a>
-        <a href="/signin"><button className="button-50">Sign In</button></a>
+        <a href="/signup"><button className="button-50 w-64">Sign Up</button></a>
+        <a href="/signin"><button className="button-50 w-64">Sign In</button></a>
       </div>
     </div>
   )
