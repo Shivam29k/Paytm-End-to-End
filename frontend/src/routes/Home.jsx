c0ecb314c9fe5649c3add7aa3d190c4dd07fc2c5
Shivam Kumar <skumarshivam50@gmail.com>
2024-03-04 11:24:01 +0530

    commit

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+  const navigate = useNavigate()
   return (
     <div className="flex items-center justify-center gap-5 h-screen">
         <div className="flex items-center justify-center gap-10 m-10 flex-col ">
         <div className="text-3xl w-64 text-center font-bold">Welcome to Payments app</div>
-        <a href="/signup"><button className="button-50 w-64">Sign Up</button></a>
-        <a href="/signin"><button className="button-50 w-64">Sign In</button></a>
+        <button onClick={()=> navigate('/signup')} className="button-50 w-64">Sign Up</button>
+        <button onClick={()=> navigate('/signin')} className="button-50 w-64">Sign In</button>
       </div>
     </div>
   )
