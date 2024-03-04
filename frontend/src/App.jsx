6c4b4ee4580f20aa49c2f7ba2d53c222730370c4
Shivam Kumar <skumarshivam50@gmail.com>
2024-03-04 07:23:04 +0530

    added home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,29 @@
 import React from "react"
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import Signup from "./routes/Signup"
 import Signin from "./routes/Signin"
 import Dashboard from "./routes/Dashboard"
 import SendMoney from "./routes/SendMoney"
 import Redirect from "./routes/Redirect"
+import Home from "./routes/Home"
 
 function App() {
 
+
   return (
-    <div>
+  
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/signin" element={<Signin/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/send" element={<SendMoney/>} />
-          <Route path="/send/redirect" element={<Redirect/>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+          <Route path="/send/redirect" element={<Redirect />} />
+          <Route path="/" element={<Home />} />
+
         </Routes>
       </BrowserRouter>
-    </div>
+
   )
 }
 
---
+++ frontend/src/routes/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="flex items-center justify-center gap-5 h-screen">
+        <div className="flex items-center justify-center gap-10 m-10 flex-col ">
+        <div className="text-3xl w-64 text-center font-bold">Welcome to Payments app</div>
+        <a href="/signup"><button className="button-50">Sign Up</button></a>
+        <a href="/signin"><button className="button-50">Sign In</button></a>
+      </div>
+    </div>
+  )
+}
+
+export default Home
